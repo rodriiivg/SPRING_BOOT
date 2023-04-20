c215723317d297110ee253230a419a44dca197d1
Rodrigo Valdés García <115140411+rodriiivg@users.noreply.github.com>
2023-04-20 10:23:34 +0200

    Add files via upload

---
+++ form/src/main/java/com/formulario_completo_con_consultas/form/upload/storage/StorageException.java
@@ -0,0 +1,13 @@
+package com.formulario_completo_con_consultas.form.upload.storage;
+
+public class StorageException extends RuntimeException {
+	
+	public StorageException(String message) {
+		super(message);
+	}
+	
+	public StorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
---
+++ form/src/main/java/com/formulario_completo_con_consultas/form/upload/storage/StorageProperties.java
@@ -0,0 +1,19 @@
+package com.formulario_completo_con_consultas.form.upload.storage;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix="storage")
+public class StorageProperties {
+
+    
+    private String location = "upload-dir";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
